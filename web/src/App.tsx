5116e4484c39363f81a997f7685362bb68d45e31
Gui <guilhermenen15@gmail.com>
2022-09-28 23:15:32 -0300

    Update app.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -73,8 +73,8 @@ function App() {
 
       <div className="pt-1 bg-nlw-gradient self-stretch mt-8 rounded-lg overflow-hidden">
         <div className="bg-[#2A2634] px-8 py-6 rounded-lg">
-          <strong>Não encontrou o seu duo?</strong>
-          <span>Publique um anúncio para encontrar novos players</span>
+          <strong className="text-2xl text-white font-black block">Não encontrou o seu duo?</strong>
+          <span className="text-zinc-400">Publique um anúncio para encontrar novos players</span>
         </div>
       </div>
     </div>
