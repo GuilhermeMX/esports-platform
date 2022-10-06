5cf01440d3563884fb99a5cb48ae79d2558c2eca
Gui <guilhermenen15@gmail.com>
2022-10-05 21:27:16 -0300

    updated html, tailconfig and app.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,8 +6,8 @@ import logoImg from './assets/logo-nlw-esports.svg';
 
 function App() {
   return (
-    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
-      <img src={logoImg} alt="" />
+    <div className="max-w-[1644px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} alt="" className="max-w-[550px] max-h-[300px] pt-10" />
 
       <h1 className="text-6xl text-white font-black mt-20">
         Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui.
@@ -66,11 +66,11 @@ function App() {
           <div className="w full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
             <strong className="font-bold text-white block">World Of Warcraft</strong>
 
-            <span className="text-zinc-300 text-sm block">4 anúncios</span> 
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
           </div>
         </a>
-      
-      
+
+
       </div>
 
       <div className="pt-1 bg-nlw-gradient self-stretch mt-8 rounded-lg overflow-hidden">
